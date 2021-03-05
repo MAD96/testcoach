8f943cbe07a075ac742988866c5a8053a0d589fe
MAD96 <andresdmb96@gmail.com>
2021-03-05 15:11:56 -0400

    figuring out how to import styles to a blade file

---
+++ resources/views/header.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/resources/css/header.css">
+    <title>Coachteen Academy</title>
+</head>
+<body>
+    
+    <div class="container-fluid navSuperior" id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-md-7 col-sm-5 col-xs-12 social">
+                    <ul>
+                        <li>
+                            <a href="http://facebook.com/coachteenu" target="_blank">
+                                <i class="fa fa-facebook socialNetwork facebook" aria-hidden="true"> Facebook</i>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="http://twitter.com/coachteenu" target="_blank">
+                                <i class="fa fa-twitter socialNetwork twitter" aria-hidden="true"></i>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="http://instagram.com/coachteenu" target="_blank">
+                                <i class="fa fa-instagram socialNetwork instagram" aria-hidden="true"></i>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="http://youtube.com/coachteenu" target="_blank">
+                                <i class="fa fa-youtube socialNetwork youtube" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <header class="container-fluid">
+
+        <div class="container">
+    
+            <div class="row">
+    
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"></div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="logo">
+                    Logo
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"></div>
+    
+            </div>
+    
+        </div>
+    
+    </header>
+    
+    
+
+    <div>
+        @yield('content')
+    </div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('header')
+
+@section('content')
+    <h1>Sample</h1>
+@endsection
